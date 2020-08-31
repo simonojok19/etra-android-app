190ac089f5c1bfa8e4ba9c714a709695c34d5f6e
simonojok19 <simonojok19@gmail.com>
2020-08-31 20:36:20 +0300

    finished parishes

--- app/src/main/java/co/veritasinteractive/pollrelay/data/models/Parish.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/data/models/Parish.kt
@@ -3,20 +3,11 @@ package co.veritasinteractive.pollrelay.data.models
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Parish(var name: String?, var parishs: Array<Parish>?): Parcelable {
+data class Parish(var name: String?, var pollingstations: Array<PollingStation>?): Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readString(),
-        parcel.createTypedArray(CREATOR)
+        parcel.createTypedArray(PollingStation)
     ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeTypedArray(parishs, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
     }
 
     override fun equals(other: Any?): Boolean {
@@ -26,18 +17,27 @@ data class Parish(var name: String?, var parishs: Array<Parish>?): Parcelable {
         other as Parish
 
         if (name != other.name) return false
-        if (parishs != null) {
-            if (other.parishs == null) return false
-            if (!parishs!!.contentEquals(other.parishs!!)) return false
-        } else if (other.parishs != null) return false
+        if (pollingstations != null) {
+            if (other.pollingstations == null) return false
+            if (!pollingstations!!.contentEquals(other.pollingstations!!)) return false
+        } else if (other.pollingstations != null) return false
 
         return true
     }
 
     override fun hashCode(): Int {
         var result = name?.hashCode() ?: 0
-        result = 31 * result + (parishs?.contentHashCode() ?: 0)
+        result = 31 * result + (pollingstations?.contentHashCode() ?: 0)
         return result
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeTypedArray(pollingstations, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
     }
 
     companion object CREATOR : Parcelable.Creator<Parish> {
@@ -49,4 +49,5 @@ data class Parish(var name: String?, var parishs: Array<Parish>?): Parcelable {
             return arrayOfNulls(size)
         }
     }
+
 }
---
+++ app/src/main/java/co/veritasinteractive/pollrelay/data/models/SubCounty.kt
@@ -0,0 +1,4 @@
+package co.veritasinteractive.pollrelay.data.models
+
+data class SubCounty(var name: String?, var parish: Array<Parish>?) {
+}

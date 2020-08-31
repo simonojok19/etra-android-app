47b82b62fe2320dafa91cfdfef7338abbb003471
simonojok19 <simonojok19@gmail.com>
2020-08-31 23:24:03 +0300

    added some fields

--- app/src/main/java/co/veritasinteractive/pollrelay/MainActivity.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/MainActivity.kt
@@ -1,9 +1,11 @@
 package co.veritasinteractive.pollrelay
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import co.veritasinteractive.pollrelay.form.FormActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +15,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+            startActivity(Intent(this, FormActivity::class.java))
         }
     }
 }

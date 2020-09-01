76f3dfc0f52cc086371951608c1bb11f996ef4f0
simonojok19 <simonojok19@gmail.com>
2020-09-01 23:08:13 +0300

    refactored the country model

--- app/src/main/java/co/veritasinteractive/pollrelay/data/models/County.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/data/models/County.kt
@@ -2,9 +2,19 @@ package co.veritasinteractive.pollrelay.data.models
 
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 
-data class County(var name: String?, var subcounties: Array<SubCounty>?): Parcelable {
+@Entity(tableName = "county")
+data class County(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int?,
+    var name: String?,
+    @Ignore
+    var subcounties: Array<SubCounty>?): Parcelable {
     constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
         parcel.readString(),
         parcel.createTypedArray(SubCounty)
     ) {
@@ -16,6 +26,7 @@ data class County(var name: String?, var subcounties: Array<SubCounty>?): Parcel
 
         other as County
 
+        if (id != other.id) return false
         if (name != other.name) return false
         if (subcounties != null) {
             if (other.subcounties == null) return false
@@ -26,12 +37,14 @@ data class County(var name: String?, var subcounties: Array<SubCounty>?): Parcel
     }
 
     override fun hashCode(): Int {
-        var result = name?.hashCode() ?: 0
+        var result = id ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
         result = 31 * result + (subcounties?.contentHashCode() ?: 0)
         return result
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
         parcel.writeString(name)
         parcel.writeTypedArray(subcounties, flags)
     }
@@ -49,5 +62,6 @@ data class County(var name: String?, var subcounties: Array<SubCounty>?): Parcel
             return arrayOfNulls(size)
         }
     }
+
 
 }

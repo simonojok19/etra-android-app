6b7f8a105ff1cf91df39f0e4ce639a86009c8cf9
simonojok19 <simonojok19@gmail.com>
2020-09-01 23:53:38 +0300

    added request queue

--- app/src/main/java/co/veritasinteractive/pollrelay/workers/UgDataDownloadWorker.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/workers/UgDataDownloadWorker.kt
@@ -3,11 +3,19 @@ package co.veritasinteractive.pollrelay.workers
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import co.veritasinteractive.pollrelay.UG_DATA_URL
 import co.veritasinteractive.pollrelay.data.PollRelayDatabase
 import co.veritasinteractive.pollrelay.data.dao.UgDataDao
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONObject
 
 class UgDataDownloadWorker(
-    context: Context,
+    val context: Context,
     workerParams: WorkerParameters): Worker(
     context,
     workerParams
@@ -16,6 +24,24 @@ class UgDataDownloadWorker(
 
 
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+       val jsonRequest = JsonObjectRequest(
+           Request.Method.GET,
+           UG_DATA_URL,
+           JSONObject(),
+           {
+
+           },
+           {
+
+           }
+
+       )
+        val cache = DiskBasedCache(context.cacheDir, 1024 * 1024)
+        val network = BasicNetwork(HurlStack())
+        val requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+        requestQueue.add(jsonRequest)
+        return Result.success()
     }
 }

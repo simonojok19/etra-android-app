66cbbeaca8d2395b99118db4e4093d77d09b77ef
simonojok19 <simonojok19@gmail.com>
2020-09-01 10:58:44 +0300

    added splash screen

--- app/src/main/java/co/veritasinteractive/pollrelay/SplashScreenActivity.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/SplashScreenActivity.kt
@@ -1,11 +1,18 @@
 package co.veritasinteractive.pollrelay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import co.veritasinteractive.pollrelay.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }, 5000)
     }
 }

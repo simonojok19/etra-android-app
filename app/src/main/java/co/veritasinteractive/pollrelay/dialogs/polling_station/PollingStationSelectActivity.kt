b2057c04aee1bc0415f1e6740fcdb36c8d2ed957
simonojok19 <simonojok19@gmail.com>
2020-09-02 21:26:55 +0300

    finished polling station selector

--- app/src/main/java/co/veritasinteractive/pollrelay/dialogs/polling_station/PollingStationSelectActivity.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/dialogs/polling_station/PollingStationSelectActivity.kt
@@ -1,12 +1,39 @@
 package co.veritasinteractive.pollrelay.dialogs.polling_station
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.veritasinteractive.pollrelay.R
+import co.veritasinteractive.pollrelay.data.models.Parish
+import co.veritasinteractive.pollrelay.data.models.PollingStation
+import kotlinx.android.synthetic.main.activity_polling_station_select.*
 
-class PollingStationSelectActivity : AppCompatActivity() {
+class PollingStationSelectActivity : AppCompatActivity(), PollingStationAdapter.OnPollingClickListener{
+
+    companion object {
+        val PARISH = "co.veritasinteractive.pollrelay.dialogs.polling_station.PollingStationSelectActivity.PARISH"
+        val POLLING_STATION = "co.veritasinteractive.pollrelay.dialogs.polling_station.PollingStationSelectActivity.POLLING_STATION"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_polling_station_select)
+
+        val recyclerView: RecyclerView = item_recycler_view
+        val parish: Parish? = intent.getParcelableExtra<Parish>(PARISH)
+        parish?.pollingstations?.let {
+            val adapter = PollingStationAdapter(it, this)
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        }
+
+    }
+
+    override fun onPollingStationClick(pollingStation: PollingStation) {
+        val intent = Intent()
+        intent.putExtra(POLLING_STATION, pollingStation)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }

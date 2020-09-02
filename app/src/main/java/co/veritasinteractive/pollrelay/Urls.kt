d076380ff79115d0004e7ba784186a9616821e30
simonojok19 <simonojok19@gmail.com>
2020-09-02 16:15:37 +0300

    added ug data

--- app/src/main/java/co/veritasinteractive/pollrelay/Urls.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/Urls.kt
@@ -1,4 +1,4 @@
 package co.veritasinteractive.pollrelay
 
 const val BASE_URL = "http://vote-ai.herokuapp.com/"
-const val UG_DATA_URL = "${BASE_URL}assets/mobile/ugData.json"
+const val UG_DATA_URL = "${BASE_URL}assets/mobile/ugdata.json"

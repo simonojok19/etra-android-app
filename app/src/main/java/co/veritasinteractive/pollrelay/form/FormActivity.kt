c598739ca393ed836dd94442e07390c1731e0829
simonojok19 <simonojok19@gmail.com>
2020-09-02 17:49:50 +0300

    added the district

--- app/src/main/java/co/veritasinteractive/pollrelay/form/FormActivity.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/form/FormActivity.kt
@@ -3,12 +3,19 @@ package co.veritasinteractive.pollrelay.form
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import co.veritasinteractive.pollrelay.R
+import co.veritasinteractive.pollrelay.data.models.District
 
 class FormActivity : AppCompatActivity() {
+    private lateinit var viewModel: FormActivityViewModel
+    private lateinit var district: District
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        viewModel = ViewModelProvider(this).get(FormActivityViewModel::class.java)
+        district = viewModel.getDistrict()
+
     }
 
     fun selectRegion(view: View) {}

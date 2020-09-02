ce7fe852be1855d9a9288a6d38e721a92fe9d8d4
simonojok19 <simonojok19@gmail.com>
2020-09-02 20:15:47 +0300

    finished adding the activity

--- app/src/main/java/co/veritasinteractive/pollrelay/dialogs/constituency/sub_county/SubCountyAdapter.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/dialogs/constituency/sub_county/SubCountyAdapter.kt
@@ -1,4 +1,45 @@
 package co.veritasinteractive.pollrelay.dialogs.constituency.sub_county
 
-class SubCountyAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import co.veritasinteractive.pollrelay.R
+import co.veritasinteractive.pollrelay.data.models.SubCounty
+
+class SubCountyAdapter (private val subCounties: Array<SubCounty>, val context: Context): RecyclerView.Adapter<SubCountyAdapter.SubCountyViewHolder>(){
+    private val onSubCountyClickListener: OnSubCountyClickListener = context as OnSubCountyClickListener
+
+    class SubCountyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var textView: TextView = itemView.findViewById<TextView>(R.id.item_name)
+        fun bind(
+            subCounty: SubCounty,
+            onSubCountyClickListener: OnSubCountyClickListener
+        ) {
+            textView.text = subCounty.name
+            textView.setOnClickListener {
+                onSubCountyClickListener.onSubCountyClick(subCounty)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubCountyViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recycler_view_item, parent, false)
+        return SubCountyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SubCountyViewHolder, position: Int) {
+        holder.bind(subCounties[position], onSubCountyClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return subCounties.size
+    }
+
+    public interface OnSubCountyClickListener {
+        fun onSubCountyClick(subCounty: SubCounty)
+    }
 }

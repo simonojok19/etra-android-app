5e4667bf6dd9425850db3f60b9901be41b56d281
simonojok19 <simonojok19@gmail.com>
2020-09-02 20:48:13 +0300

    added another activityr

---
+++ app/src/main/java/co/veritasinteractive/pollrelay/dialogs/parish/ParishSelectActivity.kt
@@ -0,0 +1,12 @@
+package co.veritasinteractive.pollrelay.dialogs.parish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.veritasinteractive.pollrelay.R
+
+class ParishSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parish_select)
+    }
+}

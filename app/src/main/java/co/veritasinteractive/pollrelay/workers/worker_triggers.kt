0be406851ca8be07ef8d3ff438ba29acbd174f47
simonojok19 <simonojok19@gmail.com>
2020-09-02 08:35:57 +0300

    added permissions internet

--- app/src/main/java/co/veritasinteractive/pollrelay/workers/worker_triggers.kt
+++ app/src/main/java/co/veritasinteractive/pollrelay/workers/worker_triggers.kt
@@ -1,21 +1,26 @@
 package co.veritasinteractive.pollrelay.workers
 
 import android.content.Context
-import androidx.work.OneTimeWorkRequest
-import androidx.work.PeriodicWorkRequest
-import androidx.work.WorkManager
-import androidx.work.WorkRequest
+import androidx.work.*
 import java.util.concurrent.TimeUnit
+
 
 fun startWorkers(context: Context) {
     startUgDataDownLoadWorker(context)
 }
 
 fun startUgDataDownLoadWorker(context: Context) {
+
+
+    val constraints: Constraints =  Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .build()
     val downloadWorkRequest: WorkRequest = PeriodicWorkRequest.Builder(
         UgDataDownloadWorker::class.java,
         60,
-        TimeUnit.SECONDS)
+        TimeUnit.SECONDS
+    )
+        .setConstraints(constraints)
         .build()
 
     WorkManager.getInstance(context).enqueue(downloadWorkRequest)
